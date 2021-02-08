f546bd860a941fa3bf36dbd19fe3e49bc4b0ebf3
Ayon Bakshi <ayon.bakshi@gmail.com>
2021-02-07 19:26:02 -0500

    formatting

--- tests/physics_test.cpp
+++ tests/physics_test.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
-#include <tuple>
 #include <memory>
+#include <tuple>
 
 #include <gtest/gtest.h>
 
-#include <utils/constants.hpp>
-#include <utils/glm.hpp>
+#include <model/material.hpp>
 #include <model/mesh.hpp>
 #include <model/meshUtils.hpp>
-#include <model/material.hpp>
 #include <model/object.hpp>
 #include <physics/physics.hpp>
 #include <render/render.hpp>
+#include <utils/constants.hpp>
+#include <utils/glm.hpp>
 
 TEST(PhysicsSuite, SphereMassPropTest) {
   float density = 3.f;
@@ -28,25 +28,20 @@ TEST(PhysicsSuite, SphereMassPropTest) {
   glm::mat3 expectedInertia{2.f * expectedMass * radius * radius / 5.f};
 
   float eps = glm::length(expectedCOM) / 500.f;
-  for (uint32_t i = 0; i < 3; ++i)
-  {
-    
+  for (uint32_t i = 0; i < 3; ++i) {
+
     EXPECT_NEAR(std::get<1>(massProps)[i], expectedCOM[i], eps);
   }
 
   eps = 0;
-  for (uint32_t c = 0; c < 3; ++c)
-  {
-    for (uint32_t r = 0; r < 3; ++r)
-    {
+  for (uint32_t c = 0; c < 3; ++c) {
+    for (uint32_t r = 0; r < 3; ++r) {
       eps += expectedInertia[c][r] / 500.f;
     }
   }
 
-  for (uint32_t c = 0; c < 3; ++c)
-  {
-    for (uint32_t r = 0; r < 3; ++r)
-    {
+  for (uint32_t c = 0; c < 3; ++c) {
+    for (uint32_t r = 0; r < 3; ++r) {
       EXPECT_NEAR(std::get<2>(massProps)[c][r], expectedInertia[c][r], eps);
     }
   }
@@ -71,30 +66,24 @@ TEST(PhysicsSuite, CubeMassPropTest) {
   EXPECT_NEAR(std::get<0>(massProps), expectedMass, fcm::eps);
 
   float eps = glm::length(expectedCOM) / 500.f;
-  for (uint32_t i = 0; i < 3; ++i)
-  {
-    
+  for (uint32_t i = 0; i < 3; ++i) {
+
     EXPECT_NEAR(std::get<1>(massProps)[i], expectedCOM[i], eps);
   }
 
   eps = 0;
-  for (uint32_t c = 0; c < 3; ++c)
-  {
-    for (uint32_t r = 0; r < 3; ++r)
-    {
+  for (uint32_t c = 0; c < 3; ++c) {
+    for (uint32_t r = 0; r < 3; ++r) {
       eps += expectedInertia[c][r] / 500.f;
     }
   }
 
-  for (uint32_t c = 0; c < 3; ++c)
-  {
-    for (uint32_t r = 0; r < 3; ++r)
-    {
+  for (uint32_t c = 0; c < 3; ++c) {
+    for (uint32_t r = 0; r < 3; ++r) {
       EXPECT_NEAR(std::get<2>(massProps)[c][r], expectedInertia[c][r], eps);
     }
   }
 }
-
 
 TEST(PhysicsSuite, IterateAngularTest) {
   EXPECT_EQ(1, 1);
